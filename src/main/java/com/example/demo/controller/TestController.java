62efb9883b56a08d34d5018ecadfefef22764217
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2020-10-28 08:38:22 +0700

    initial project

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TestController {
+    @GetMapping(value = "/products")
+    public List<String> getProducts() {
+        List<String> productsList = new ArrayList<>();
+        productsList.add("Honey");
+        productsList.add("Almond");
+        return productsList;
+    }
+
+    @PostMapping(value = "/products")
+    public String createProduct() {
+        return "Product is saved successfully";
+    }
+}
